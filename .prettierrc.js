56992d11cf1e42c28ea6ef57dee019865c0aaaa3
tiavina-mika <ralainirinatiavina@gmail.com>
2020-08-15 16:46:15 +0300

    remove v2

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    semi: true,
-    trailingComma: "es5",
-    bracketSpacing: true,
-    singleQuote: true,
-    jsxBracketSameLine: true,
-    printWidth: 120,
-    tabWidth: 4
-};
