3b123fa0e493dac7d1e84766d563cd439ec31913
tiavina-mika <ralainirinatiavina@gmail.com>
2020-08-23 12:19:00 +0300

    change file name

--- components/home/competences/index.tsx
+++ components/home/competences/index.tsx
@@ -3,7 +3,7 @@ import { createUseStyles } from 'react-jss';
 import Plx from 'react-plx';
 import clsx from 'clsx';
 
-import CompetencesSvg from './svg/CompetencesSvg';
+import CompetencesSvg from './svg';
 import Cards from './cards';
 import WebIcon from './svg/WebIcon';
 import DatabaseIcon from './svg/DatabaseIcon';
