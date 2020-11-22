587d0b76338050966543695dd7949649cf76d0a6
tiavina-mika <tiavinamika@gmail.com>
2020-11-22 12:29:12 +0300

    dynamize projects

--- components/home/projects-grid/ProjectGrid.tsx
+++ components/home/projects-grid/ProjectGrid.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { createUseStyles } from 'react-jss';
+
 import Text from '../../shared/Text';
 import Title from '../../shared/Title';
+import { ProjectI } from './ProjectsGrid';
 
 const useStyles = createUseStyles((theme: any) => ({
     block: {
@@ -11,9 +13,9 @@ const useStyles = createUseStyles((theme: any) => ({
         transition: 'all .5s cubic-bezier(.19,1,.4,1)',
         width: 580,
         height: 590,
-        backgroundColor: '#d3b589',
+        backgroundColor: ({ color }: Pick<ProjectI, 'color'>) => '#' + color,
         marginBottom: theme.spacing(6),
-        color: '#fff',
+        color: ({ isDark }) => (isDark ? '##54575a' : '#fff'),
     },
     center: {
         composes: 'stretchSelf flex1',
@@ -36,16 +38,23 @@ const useStyles = createUseStyles((theme: any) => ({
         gridTemplateRows: 'repeat(3, auto) 1fr',
         gridGap: theme.spacing(3),
         '&:hover': {
+            // the see more button
             '& div:last-child div': {
                 backgroundColor: '#fff',
                 transform: `translate(${theme.spacing(0)}px,-${theme.spacing(2)}px)`,
                 color: theme.color.secondary,
             },
+            // grey right arrow
             '& img:last-child': {
                 visibility: 'visible',
             },
+            // white right arrow
             '& img:nth(2)': {
                 visibility: 'hidden',
+            },
+            // description block animation
+            '& div:nth-of-type(3)': {
+                opacity: 1,
             },
         },
     },
@@ -58,6 +67,9 @@ const useStyles = createUseStyles((theme: any) => ({
     },
     descriptionContainer: {
         width: '40%',
+        lineHeight: 1.43,
+        opacity: 0,
+        transition: 'opacity .5s cubic-bezier(.19,1,.4,1)',
     },
     description: {},
     seeMore: {
@@ -78,32 +90,22 @@ const useStyles = createUseStyles((theme: any) => ({
     },
 }));
 
-type Props = {
-    title: string;
-    subtitle: string;
-    url: string;
-};
-
-const ProjectGrid = () => {
-    const classes = useStyles();
+const ProjectGrid: FC<ProjectI> = ({ title, subtitle, url, color, description }) => {
+    const classes = useStyles({ color: color.color, isDark: color.dark });
 
     return (
         <div className={classes.block}>
             <div className={classes.center}>
                 <div className={classes.content}>
-                    <a href="/" className={classes.cardLink} />
+                    <a href={url} className={classes.cardLink} />
                     <div>
-                        <Title text="Some card subtitle" level={4} className={classes.subtitle} />
+                        <Title text={subtitle} level={4} className={classes.subtitle} />
                     </div>
                     <div className={classes.title}>
-                        <Title text="Some card title, two line card title" level={3} />
+                        <Title text={title} level={3} />
                     </div>
                     <div className={classes.descriptionContainer}>
-                        <Text
-                            text="Some long, very very long card description, and very very long card description"
-                            tagName="p"
-                            className={classes.description}
-                        />
+                        <Text text={description} tagName="p" className={classes.description} />
                     </div>
                     <div className={classes.seeMore}>
                         <div className={classes.seeMoreButton}>
--- components/home/projects-grid/ProjectsGrid.tsx
+++ components/home/projects-grid/ProjectsGrid.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { createUseStyles } from 'react-jss';
+
 import Layout from '../../shared/Layout';
 import Text from '../../shared/Text';
 import Title from '../../shared/Title';
@@ -43,14 +44,83 @@ const useStyles = createUseStyles((theme: any) => ({
     },
 }));
 
-// type Props = {
-//     reverse?: boolean;
-//     reverseIndex: number;
-// };
+interface ColorI {
+    color: string;
+    dark?: boolean;
+}
+export interface ProjectI {
+    id: string;
+    subtitle: string;
+    title: string;
+    description: string;
+    url: string;
+    color: ColorI;
+}
+const colors: ColorI[] = [
+    { color: 'd3b589' },
+    { color: '825c79' },
+    { color: 'eeedea', dark: true },
+    { color: 'b5d4cc' },
+    { color: '7ca5cf' },
+];
+const projects: Omit<ProjectI, 'color'>[] = [
+    {
+        id: '1',
+        subtitle: 'Some card subtitle',
+        title: 'Some card title, two line card title',
+        description: 'Some long, very very long card description, and very very long card description',
+        url: '/',
+    },
+    {
+        id: '2',
+        subtitle: 'Some card subtitle 2',
+        title: 'Some card title, two line card title 2',
+        description: 'Some long, very very long card description, and very very long card description 2',
+        url: '/',
+    },
+    {
+        id: '3',
+        subtitle: 'Some card subtitle 3',
+        title: 'Some card title, two line card title 3',
+        description: 'Some long, very very long card description, and very very long card description 3',
+        url: '/',
+    },
+    {
+        id: '4',
+        subtitle: 'Some card subtitle 4',
+        title: 'Some card title, two line card title 4',
+        description: 'Some long, very very long card description, and very very long card description 4',
+        url: '/',
+    },
+    {
+        id: '5',
+        subtitle: 'Some card subtitle 5',
+        title: 'Some card title, two line card title 5',
+        description: 'Some long, very very long card description, and very very long card description 5',
+        url: '/',
+    },
+];
 
 const ProjectsGrid = () => {
     const classes = useStyles();
+    const [leftCardsData, setLeftCardData] = useState<ProjectI[]>([]);
+    const [rightCardsData, setRightCardData] = useState<ProjectI[]>([]);
 
+    useEffect(() => {
+        const split: number = projects.length <= 5 ? 2 : 3;
+        const newProjects = projects.map(
+            (project: Omit<ProjectI, 'color'>, index: number): ProjectI => ({
+                ...project,
+                color: colors[index],
+            })
+        );
+        const leftCards: ProjectI[] = [...newProjects].splice(0, split);
+        const rightCards: ProjectI[] = [...newProjects].splice(split, projects.length);
+        setLeftCardData([...leftCards]);
+        setRightCardData([...rightCards]);
+    }, []);
+
+    const splicedPorject = (project: ProjectI): ReactNode => <ProjectGrid key={project.id} {...project} />;
     return (
         <Layout className={classes.projectsRoot}>
             <div className={classes.projectsContent}>
@@ -65,19 +135,9 @@ const ProjectsGrid = () => {
                             />
                         </div>
                     </div>
-                    <ProjectGrid />
-                    {/* <div className={classes.block}>
-                        <Title text="Block1" level={4} />
-                    </div> */}
+                    {leftCardsData.map(splicedPorject)}
                 </div>
-                <div className={classes.right}>
-                    {/* <div className={classes.block}>
-                        <Title text="Block1" level={4} />
-                    </div>
-                    <div className={classes.block}>
-                        <Title text="Block1" level={4} />
-                    </div> */}
-                </div>
+                <div className={classes.right}>{rightCardsData.map(splicedPorject)}</div>
             </div>
         </Layout>
     );
